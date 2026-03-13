9e4487069ce72b1cef8823df00c6a09c8081b2bc
Tino Waldner <tino.waldner@Tinos-MBP.home>
2026-03-13 11:57:31 +0100

    ENH: Add Size and Modification Date to Artifactory Pane

--- core/api_client.py
+++ core/api_client.py
@@ -1,32 +1,50 @@
 from auth import AuthSession
+from artifactory import ArtifactoryPath
+from datetime import datetime
 
 
 class ArtifactoryAPI:
-
     def __init__(self, config):
         self.session = AuthSession(
             config["server"],
             config["user"],
             config["token"]
         )
+        self.base_url = config["server"].rstrip('/')
+        self.apikey = config["token"].rstrip('/')
 
     def list_repositories(self):
         data = self.session.get("/api/repositories")
         return [r["key"] for r in data]
 
     def list_folder(self, repo, path=""):
-        url = f"/api/storage/{repo}/{path}"
-        data = self.session.get(url)
+        repo_path = f"{repo}/{path.lstrip('/').rstrip('/')}"
+        full_url = f"{self.base_url}/{repo_path}"
+
+        folder = ArtifactoryPath(
+            full_url,
+            apikey = self.apikey
+        )
+
         items = []
 
-        for c in data.get("children", []):
-            items.append(
-                {
-                    "name": c["uri"].lstrip("/"),
-                    "is_dir": c["folder"],
-                }
-            )
+        for child in folder.iterdir():
+            st = child.stat()
+            size = st.size if not st.is_dir else 0
 
+            if isinstance(st.mtime, (int, float)):
+               modified = datetime.fromtimestamp(st.mtime).isoformat()
+            elif isinstance(st.mtime, datetime):
+               modified = st.mtime.isoformat()
+            else:
+               modified = None
+
+            items.append({
+                "name": child.name,
+                "is_dir": st.is_dir,
+                "size": size,
+                "modified": modified,
+            })
         return items
 
     def properties(self, repo, path):
