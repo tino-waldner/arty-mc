fe0717791ea3c6bfb102c9c97f858ef0e33ba593
Tino Waldner <tino.waldner@Tinos-MBP.home>
2026-03-15 16:18:18 +0100

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="arty-mc",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "textual",
