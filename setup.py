0b8c7a156f5c405db160e5425a50626b23590eb5
Tino Waldner <tino.waldner@Tinos-MBP.home>
2026-03-17 21:25:10 +0100

    Bump Version 1.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup  # type: ignore
 
 setup(
     name="arty-mc",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=[
         "textual",
