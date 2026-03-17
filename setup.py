8a11cf1c966e04f6ec50bc7e5d7cd01e2f793aff
Tino Waldner <tino.waldner@Tinos-MBP.home>
2026-03-17 21:00:22 +0100

    FIX: setup.py use SPDX license expression

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/tino-waldner/arty-mc",
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Utilities",
         "Environment :: Console",
